ccb5076e8a11e03bf819f24a016cb51141cef218
learner-enthusiast <loveumearnab.2812000@gmail.com>
2026-03-28 12:18:20 +0530

    fix: nothing

--- app/index.tsx
+++ app/index.tsx
@@ -5,30 +5,40 @@ import TaskCard from "@/components/TaskCard";
 import Colors from "@/constants/Colors";
 import { FilterOptions, TASKS } from "@/constants/tasks";
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Index = () => {
   const insets = useSafeAreaInsets();
   const [activeFilter, setActiveFilter] = useState<FilterOptions>("All");
+  const [filteredTasks, setFilteredTasks] = useState(TASKS);
+
+  const filterMap = {
+    "To do": "To-do",
+    Completed: "Done",
+
+    "In Progress": "In Progress",
+  };
+  useEffect(() => {
+    setFilteredTasks(
+      activeFilter === "All"
+        ? TASKS
+        : TASKS.filter((task) => task.status === filterMap[activeFilter]),
+    );
+  }, [activeFilter]);
   return (
     <View style={[styles.container, { paddingTop: insets.top }]}>
       <StatusBar style="light" />
+      {/* Header components */}
+      <Header />
+      <DateSelector />
+      <FilterTabs selected={activeFilter} onSelect={setActiveFilter} />
+
       <FlatList
-        data={TASKS}
+        data={filteredTasks}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <TaskCard task={item} />}
-        ListHeaderComponent={
-          <>
-            {/* Header */}
-            <Header />
-            {/* DateSelector */}
-            <DateSelector />
-            {/* FiltersTabs */}
-            <FilterTabs selected={activeFilter} onSelect={setActiveFilter} />
-          </>
-        }
         contentContainerStyle={styles.list}
         showsVerticalScrollIndicator={false}
       />
@@ -40,8 +50,10 @@ export default Index;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: "column",
     backgroundColor: Colors.background,
+    flex: 1,
+    gap: 8,
   },
   list: {
     paddingBottom: 24,
